d6613801bd7d3e4b83fc9ac05c097be4363ed744
Edimar <edimarbmjunior@gmail.com>
2018-03-07 21:11:10 -0300

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  //tab2Root = AboutPage; 
+  //tab3Root = ContactPage;
+  tab4Root = FeedPage;
+  //tab5Root = IntroPage;
+
+  constructor() {
+
+  }
+}
